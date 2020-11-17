1f0aac6ab91a3566fc5f5b247c541c78f375a903
Angus McLeod <angus@mcleod.org.au>
2020-11-17 19:41:23 +1100

    Bugfix update_bumped_at

--- lib/layouts/category.rb
+++ lib/layouts/category.rb
@@ -2,7 +2,9 @@ class ::DiscourseLayouts::Category
   def self.update_bumped_at(category)
     return unless category
     
-    if post = Post.find_by_id(category.latest_post_id)
+    post = Post.find_by_id(category.latest_post_id)
+    
+    if post && post.respond_to?('topic') && post.topic
       category.custom_fields["bumped_at"] = post.topic.bumped_at
       category.save_custom_fields(true)
     end
