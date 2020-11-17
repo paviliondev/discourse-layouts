b0f55a5a3fd57c3757bd4b3b78ec0de3b7e109d8
Angus McLeod <angus@mcleod.org.au>
2020-11-17 19:24:36 +1100

    Change category bumped_at to topic of latest post

--- lib/layouts/category.rb
+++ lib/layouts/category.rb
@@ -2,8 +2,8 @@ class ::DiscourseLayouts::Category
   def self.update_bumped_at(category)
     return unless category
     
-    if topic = Topic.find_by_id(category.latest_topic_id)
-      category.custom_fields["bumped_at"] = topic.bumped_at
+    if post = Post.find_by_id(category.latest_post_id)
+      category.custom_fields["bumped_at"] = post.topic.bumped_at
       category.save_custom_fields(true)
     end
   end
