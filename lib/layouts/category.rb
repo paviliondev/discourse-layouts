741bb260a1dc553b7c33e45d554e19b1d6449af9
Angus McLeod <angus@mcleod.org.au>
2021-01-12 10:55:38 +1100

    Take latest of topic updated_at and bumped_at for category bumped_at

--- lib/layouts/category.rb
+++ lib/layouts/category.rb
@@ -5,7 +5,8 @@ class ::DiscourseLayouts::Category
     post = Post.find_by_id(category.latest_post_id)
     
     if post && post.respond_to?('topic') && post.topic
-      category.custom_fields["bumped_at"] = post.topic.bumped_at
+      latest_date = [post.topic.bumped_at, post.topic.updated_at].compact.max
+      category.custom_fields["bumped_at"] = latest_date
       category.save_custom_fields(true)
     end
   end
