aa633f155da1d7c57d98948f0b4f4b074d02ea97
Angus McLeod <angus@mcleod.org.au>
2022-08-12 15:47:36 +0100

    Update plugin metadata

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,21 @@
 # name: discourse-layouts
 # about: A framework for custom Discourse layouts.
 # version: 0.2
-# authors: Angus McLeod
+# authors: Angus McLeod, Keegan George
+# contact_emails: development@pavilion.tech
 # url: https://github.com/paviliondev/discourse-layouts
 
 register_asset 'stylesheets/common/layouts.scss'
 
 if respond_to?(:register_svg_icon)
-  register_svg_icon "caret-right" 
+  register_svg_icon "caret-right"
   register_svg_icon "caret-down"
   register_svg_icon "save"
 end
 
 enabled_site_setting :layouts_enabled
 
-after_initialize do  
+after_initialize do
   %w[
     ../lib/layouts/engine.rb
     ../lib/layouts/widget.rb
@@ -26,24 +27,24 @@ after_initialize do
   ].each do |key|
     load File.expand_path(key, __FILE__)
   end
-  
+
   add_to_serializer(:site, :layout_widgets) do
     ActiveModel::ArraySerializer.new(
       DiscourseLayouts::Widget.list(guardian: scope),
         each_serializer: DiscourseLayouts::WidgetSerializer
     )
   end
-  
+
   ### Discourse modifications for widgets
-  
+
   TopicQuery.public_valid_options.push(:no_definitions, :limit, :per_page)
-  
+
   #### Add bumped_at to categories and serialize it with site categories
-  
+
   ::Category.prepend LayoutsCategoryExtension
   ::Category.singleton_class.prepend LayoutsCategoryClassExtension
   ::PostCreator.prepend LayoutsPostCreatorExtension
-  
+
   Site.preloaded_category_custom_fields << "bumped_at"
   add_to_serializer(:site_category, :bumped_at) { object.custom_fields['bumped_at'] }
 end
